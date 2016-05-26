2b7bdd5dd1f0bc8262c98b731b3cf7d28a6c5e73
Iliya Devedjiev <idevedjiev@hotmail.com>
2016-05-26 18:09:41 +0300

    Builded changes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JSFAQ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JoopSOFT FAQ Module")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JoopSOFT Ltd.")]
 [assembly: AssemblyProduct("JSFAQ")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
